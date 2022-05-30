6b9e986ce83a67c5cfd5e315299a024d69af6abe
ccsc <chandrachudsingh81@gmail.com>
2022-05-30 22:20:19 +0530

    Project Birthday Reminder added

---
+++ src/projects/BirthdayReminder.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import { data } from "./data";
+
+const BirthdayReminder = () => {
+  const [dogs, setDogs] = useState(data);
+  let count = dogs.length;
+  if (count == 0) {
+    count = "No";
+  }
+
+  const removeDog = (id) => {
+    let newDogs = dogs.filter((dog) => dog.id !== id);
+    setDogs(newDogs);
+  };
+  const removeAllDogs = () => {
+    setDogs([]);
+  };
+
+  return (
+    <>
+      <h1 className="dayHeading">{count} Birthdays Today</h1>
+      {dogs.map((dog) => {
+        const { id, name, age, img } = dog;
+        return (
+          <div key={id} className="dog">
+            <img className="dogImg" src={img} alt="" />
+            <div className="dogInfo">
+              <h1 className="dogName">{name}</h1>
+              <h3 className="dogAge">{age} years</h3>
+            </div>
+            <button className="removeBtn btn" onClick={() => removeDog(id)}>
+              X
+            </button>
+          </div>
+        );
+      })}
+      <button className="removeAllBtn btn" onClick={removeAllDogs}>
+        Remove All
+      </button>
+    </>
+  );
+};
+
+export default BirthdayReminder;
---
+++ src/projects/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    name: "Darcy",
+    age: 5,
+    img: "https://www.mydogsname.com/wp-content/uploads/2016/02/West-Highland-White-Terrier-thumb.jpg",
+  },
+  {
+    id: 2,
+    name: "Maroon",
+    age: 8,
+    img: "https://www.mydogsname.com/wp-content/uploads/2016/02/beagle-thumb.jpg",
+  },
+  {
+    id: 3,
+    name: "Sharon",
+    age: 3,
+    img: "https://www.mydogsname.com/wp-content/uploads/2016/02/yorkshire-terrier-thumbs.jpg",
+  },
+  {
+    id: 4,
+    name: "Kaska",
+    age: 4,
+    img: "https://www.mydogsname.com/wp-content/uploads/2016/02/golden-retriever-thumb.jpg",
+  },
+  {
+    id: 5,
+    name: "Max",
+    age: 6,
+    img: "https://www.mydogsname.com/wp-content/uploads/2016/02/siberian-husky-thumb.jpg",
+  },
+];
