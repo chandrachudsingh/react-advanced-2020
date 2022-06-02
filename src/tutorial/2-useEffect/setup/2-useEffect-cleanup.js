ad95ecdda7e4da85399c357744e7cad9bdb9985d
ccsc <chandrachudsingh81@gmail.com>
2022-06-02 19:26:12 +0530

    useEffect cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("resize", checkSize);
+    // return () => {
+    //   console.log("cleanup");
+    //   window.removeEventListener("resize", checkSize); //to avoid memory leak due to adding mulitple event listeners (for every value) when we resize it everytime.
+    // };
+  }, []); // or we use [] in dependency to add eventListener only once at initiall rendering
+  console.log("render");
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
