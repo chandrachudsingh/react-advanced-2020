1ed3ea749e557695cd6cf6ec20c3969dedf47608
ccsc <chandrachudsingh81@gmail.com>
2022-06-03 00:26:38 +0530

    Conditional Rendering - Short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("ccsc");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+
+      {/* We cannot use if statement in return in JSX
+       {if(){console.log('hello world')}} */}
+
+      <h1>{text || "singh"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <p>there is no error</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
